1c4c9bc9550eff6b5f8c93b5e1aaca36b9e08228
Amitai <amivids603+gh@gmail.com>
2024-09-09 23:57:15 -0500

    update; added .env to support custom options such as address, port, session name

--- mc-server-agent/agent.go
+++ mc-server-agent/agent.go
@@ -2,24 +2,43 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"os"
 	"os/exec"
+	"strings"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
-const TmuxSessionName string = "0";
+var session_name string
 
 type player struct {
     Username string `json:"username"`
 }
 
 func main() {
-    //gin.SetMode(gin.ReleaseMode)
-    r := gin.Default()
+    err := godotenv.Load()
+    if err != nil {
+        log.Fatal("Error loading .env file")
+        return
+    }
 
+    address := os.Getenv("ADDRESS")
+    port := os.Getenv("PORT")
+    release_mode := os.Getenv("RELEASE_MODE")
+    session_name = os.Getenv("TMUX_SESSION_NAME")
+
+
+    if strings.ToLower(release_mode) == "true" {
+        gin.SetMode(gin.ReleaseMode)
+    }
+    r := gin.Default()
+    r.SetTrustedProxies(nil)
     r.POST("/addUser", addUser)
-    r.Run("localhost:8080")
+    uri := fmt.Sprintf("%s:%s",address, port)
+    r.Run(uri)
 }
 
 func addUser(c *gin.Context){
@@ -50,7 +69,7 @@ func addUser(c *gin.Context){
 func runTmuxCommand(uname string) error {
 
     command := fmt.Sprintf("/whitelist add %s", uname)
-    cmd := exec.Command("tmux", "send-keys", "-t", TmuxSessionName, command, "C-m")
+    cmd := exec.Command("tmux", "send-keys", "-t", session_name, command, "C-m")
 
     if err := cmd.Run(); err != nil {
         return fmt.Errorf("failed to run tmux command: %v", err)
