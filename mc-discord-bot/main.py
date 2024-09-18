a67b6d3b989bb112ccc7f2b2cc6cc46143aae326
amitai-debiche <amivids603@gmail.com>
2024-09-17 20:29:19 -0500

    update; discord bot

--- mc-discord-bot/main.py
+++ mc-discord-bot/main.py
@@ -1,7 +1,10 @@
 from typing import Final
 import os
 from dotenv import load_dotenv
+import discord
 from discord import Intents, Client, Message
+from discord.ext import commands
+import requests
 
 
 
@@ -18,6 +21,14 @@ intents.message_content = True
 client: Client = Client(intents=intents)
 channel = client.get_channel(12345)
 
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.command()
+async def set_channel(ctx):
+    channel = ctx.channel
+    await ctx.send("SETUP: Channel has been set for receiving bot authorization messages")
+
+
 
 async def send_message(username: str, message: str) -> None:
     if not username:
@@ -28,9 +39,25 @@ async def send_message(username: str, message: str) -> None:
         output: str = f"Request for {username} to be added to the-styx!"
         if message is not None:
             output += f"\nUser included the following message: `{message}`"
-##        await channel.send(output)
+        sent_message = await channel.send(output)
+        message_id = sent_message.id 
+        reaction = await bot.wait_for
+
     except Exception as e:
         print(e)
 
+@bot.event
+async def on_message(msg): 
+    def check(reaction, user):
+        role = discord.utils.get(msg.guild.roles, name="trusted")
+        return (role in reaction.roles)
 
-https://stackoverflow.com/questions/65090668/reaction-check-with-discord-py
+    if msg.author != bot.user or "SETUP" in msg.content:
+        return 
+    await bot.wait_for("reaction_add", check=check)
+    ## SEND REQUEST BACK TO BACKEND
+
+
+    r.POST("/approveUsername", handleUserApproval)
+
+##https://stackoverflow.com/questions/65090668/reaction-check-with-discord-py
